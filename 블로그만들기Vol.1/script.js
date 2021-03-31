3b443a0405e3975b05b0634373febbc0f903da46
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-03-31 12:03:14 +0900

    Update script.js

--- 블로그만들기Vol.1/script.js
+++ 블로그만들기Vol.1/script.js
@@ -1,5 +1,3 @@
-
-
 function setMenu(_menu){
     var menus=document.querySelectorAll("nav li");
     menus.forEach(function (menu){
@@ -9,3 +7,9 @@ function setMenu(_menu){
     document.querySelector("nav li."+_menu).classList.add("on");
     document.querySelector("main").className=_menu;
 }
+function setLength(){
+   document.querySelector("span.descLength").innerHTML
+    =document.querySelector("input.description")
+    .value.length+"/20";
+}
+  
