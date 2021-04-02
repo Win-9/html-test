36abfe1a1f0cdf30191b92867a39dede7910da8e
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-04-02 15:31:59 +0900

    Update script.js

--- 블로그만들기Vol.1/script.js
+++ 블로그만들기Vol.1/script.js
@@ -22,13 +22,29 @@ function showInfo(){
 
     document.querySelectorAll("#myinfo input[type=checkbox]").
     forEach(function(checkbox){
-        checkebox.checked=false;
+        checkbox.checked=false;
     });
 
     my_info.interest.forEach(function(interest){
-        document.querySelector("#myinfo input[type=radio][value="+
-        my_info.as+"]").checked=true;
+        document.querySelector("#myinfo input[type=checkbox][value="+
+        interest+"]").checked=true;
     });
+}
+function setEditMyInfo(on){
+    document.querySelector("#myinfo > div").className=on?'edit':'non-edit';
+    document.querySelectorAll("#myinfo input").forEach(function(input){
+        input.disabled=on;
+    });
+    showInfo();
+}
+function updateMyInfo(){
+    var interests=[];
+    my_info.introduction=document.querySelector("#ip-intro").value;
+    my_info.as=document.querySelector("#myinfo input[type=checkbox]:checked").forEach(function (checkbox){
+        interests.push(checked.value);
+    });
+    my_info.interest=interests;
+    setEditMyInfo(false);
 }
 function init(){
     showInfo();
