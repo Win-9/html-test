685dc5250b86189ac686b306a5aef3fec8469b3d
Win-9 <80390524+Win-9@users.noreply.github.com>
2021-04-24 21:55:48 +0900

    Create data.js

---
+++ 블로그만들기Vol.1/data.js
@@ -0,0 +1,84 @@
+var my_info = {
+  id: 'VVS',
+  user_name: "VVS!",
+  introduction: "12사단 포병여단 89포병대대 2포대.",
+  as: "pro",
+  interest: ["nature", "thing"],
+  like: [2, 5, 9],
+  follow: ['photo-luv', 'pixa', 'googler', 'js-master']
+}
+
+var photos = [
+  {
+    idx: 1,
+    user_id: 'js-master',
+    user_name: '응우옌띠옌',
+    file_name: 'photo_1.jpg',
+    description: '미사일쏘겟따우',
+    likes: 1,
+  },
+  {
+    idx: 2,
+    user_id: 'googler',
+    user_name: 'Wassup',
+    file_name: 'photo_2.jpg',
+    description: '빵소겠디',
+    likes: 3,
+  },
+  {
+    idx: 3,
+    user_id: 'mozilla',
+    user_name: '무야호~',
+    file_name: 'photo_3.jpg',
+    description: '날리겠소',
+    likes: 2,
+  },
+  {
+    idx: 4,
+    user_id: 'hackerman',
+    user_name: '그만큼',
+    file_name: 'photo_4.jpg',
+    description: '미사일쏘겠따우',
+    likes: 5,
+  },
+  {
+    idx: 5,
+    user_id: 'pixa',
+    user_name: '신나시는거지',
+    file_name: 'photo_5.jpg',
+    description: '빵쏘겠디',
+    likes: 9,
+  },
+  {
+    idx: 6,
+    user_id: 'com-al-mot',
+    user_name: '앵귀리지',
+    file_name: 'photo_6.jpg',
+    description: '날리겠소',
+    likes: 4,
+  },
+  {
+    idx: 7,
+    user_id: 'photo-luv',
+    user_name: '한인회관',
+    file_name: 'photo_7.jpg',
+    description: '이북엔핵도있어',
+    likes: 7,
+  },
+  {
+    idx: 8,
+    user_id: 'FanaKim',
+    user_name: 'TUJ',
+    file_name: 'photo_8.jpg',
+    description: '음 맛있다',
+    likes: 2,
+  },
+  {
+    idx: 9,
+    user_id: 'coder',
+    user_name: '무한~',
+    file_name: 'photo_9.jpg',
+    description: '마트 다녀오셨어요?',
+    likes: 4,
+  }
+]
